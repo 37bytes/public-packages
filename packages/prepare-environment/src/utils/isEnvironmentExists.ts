1fb7b72cf736c43cbe0ca624724cf5cf192e04dc
Ruslan Ispaev <rispaev@37bytes.com>
2023-07-05 15:32:52 +0500

    console.log for files in environments folder

--- packages/prepare-environment/src/utils/isEnvironmentExists.ts
+++ packages/prepare-environment/src/utils/isEnvironmentExists.ts
@@ -4,6 +4,8 @@ const isEnvironmentExists = (targetEnvironment, pathToEnvironmentsFolder) => {
     // Получаем все env-файлы из папки с окружениями
     const files = fs.readdirSync(pathToEnvironmentsFolder);
 
+    console.log(`environments found in environments folder ${files}`);
+
     // Проверяем есть ли среди них нужный
     return files.some((environment) => {
         return environment.endsWith(`.${targetEnvironment}`);
