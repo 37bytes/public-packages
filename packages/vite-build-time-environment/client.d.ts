b60e8f7d7c16c79995c5be291f220abe769762f4
dc-nikolaev <dnikolaev@37bytes.com>
2024-01-16 15:39:17 +0500

    @37bytes/vite-build-time-environment
    Signed-off-by: dc-nikolaev <dnikolaev@37bytes.com>

---
+++ packages/vite-build-time-environment/client.d.ts
@@ -0,0 +1,8 @@
+// vite-env.d.ts
+declare const PACKAGE_INFO_FIELD_NAME: string;
+declare const PACKAGE_INFO_FIELD_VERSION: string;
+
+declare const BUILD_TIME_VARIABLES_VERSION: string;
+declare const BUILD_TIME_VARIABLES_BRANCH: string;
+declare const BUILD_TIME_VARIABLES_COMMIT_HASH: string;
+declare const BUILD_TIME_VARIABLES_RELEASE_NAME: string;
