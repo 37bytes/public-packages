570fb0b372d6b21c5865f7aba6f263568b61d7b9
dc-nikolaev <dnikolaev@37bytes.com>
2024-02-12 12:21:56 +0500

    @37bytes/vite-dynamic-environments@0.0.3
    Signed-off-by: dc-nikolaev <dnikolaev@37bytes.com>

--- packages/vite-dynamic-environments/src/dynamicEnvironmentsSupport.ts
+++ packages/vite-dynamic-environments/src/dynamicEnvironmentsSupport.ts
@@ -4,7 +4,11 @@ import { readFileSync, readdirSync, statSync, rmSync, existsSync, mkdirSync, wri
 import { resolve, join } from 'node:path';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import dotenv from 'dotenv';
-import { Plugin } from 'vite';
+import { ConfigEnv, createLogger, Plugin } from 'vite';
+
+const logger = createLogger('info', { prefix: '[dynamicEnvironmentsSupport]' });
+
+const { FORCE_ENVIRONMENT } = process.env;
 
 interface GetDevelopmentDynamicEnvironmentParams {
     path: string;
@@ -15,7 +19,11 @@ interface GetDevelopmentDynamicEnvironmentParams {
 const getLast = <T>(array: T[]): T => array[array.length - 1];
 
 // todo отдельная функция плюс тесты
-const getDynamicEnvironment = ({ path, ignorePrefixes }: GetDevelopmentDynamicEnvironmentParams) => {
+const getDynamicEnvironment = ({ path, ignorePrefixes = [] }: GetDevelopmentDynamicEnvironmentParams) => {
+    if (!existsSync(path)) {
+        throw new Error(`File does not exist (${path})`);
+    }
+
     const config = dotenv.parse(readFileSync(path));
 
     // noinspection UnnecessaryLocalVariableJS
@@ -27,9 +35,8 @@ const getDynamicEnvironment = ({ path, ignorePrefixes }: GetDevelopmentDynamicEn
 };
 
 // todo отдельная функция плюс тесты
-const generateScript = (environment: object) => `
-  window.dynamicEnvironment = Object.freeze(${JSON.stringify(environment)});
-`;
+const generateScript = (environment: object) =>
+    `window.dynamicEnvironment = Object.freeze(${JSON.stringify(environment)});`;
 
 const DEFAULT_SCRIPT_LINK = '/dynamicEnvironment.js';
 const DEFAULT_DYNAMIC_ENVIRONMENTS_DIR = resolve(process.cwd(), 'environments/dynamic');
@@ -39,7 +46,7 @@ interface Params {
     scriptLink?: string;
     ignorePrefixes?: string[];
     dynamicEnvironmentsDir?: string;
-    outputDir?: string;
+    envOutputDir?: string;
 }
 
 // todo каждый обработчик в отдельный файл
@@ -47,63 +54,98 @@ const dynamicEnvironmentsSupport = ({
     scriptLink = DEFAULT_SCRIPT_LINK,
     ignorePrefixes = ['VITE_'],
     dynamicEnvironmentsDir = DEFAULT_DYNAMIC_ENVIRONMENTS_DIR,
-    outputDir = DEFAULT_OUTPUT_DIR
-}: Params = {}): Plugin => ({
-    name: '@37bytes/vite-dynamic-environments',
-    // режим разработки
-    configureServer(server) {
-        server.middlewares.use(scriptLink, (request, response) => {
-            // генерирует ответ каждый запрос для поддержки динамического обновления
-            const dynamicEnvironment = getDynamicEnvironment({
-                ignorePrefixes,
-                path: resolve(process.cwd(), '.env.development')
+    envOutputDir = DEFAULT_OUTPUT_DIR
+}: Params = {}): Plugin => {
+    // директория с собранным проектом
+    let outDir;
+    // директория с .env-файлами
+    let envDir;
+    let envConfig: ConfigEnv;
+
+    let forceEnvironmentScriptContent: string;
+
+    return {
+        name: '@37bytes/vite-dynamic-environments',
+        config: (config, nextEnvConfig) => {
+            // https://vitejs.dev/config/build-options.html#build-outdir
+            outDir = config.build?.outDir ?? 'dist';
+            envConfig = nextEnvConfig;
+            if (config.envDir) {
+                envDir = resolve(process.cwd(), config.envDir);
+            } else {
+                envDir = process.cwd();
+            }
+        },
+        // режим разработки
+        configureServer(server) {
+            server.middlewares.use(scriptLink, (request, response) => {
+                if (!envDir) {
+                    throw new Error('envDir is falsy');
+                }
+
+                // генерирует ответ каждый запрос для поддержки динамического обновления
+                const dynamicEnvironment = getDynamicEnvironment({
+                    ignorePrefixes,
+                    path: resolve(envDir, `.env.${envConfig.mode}`)
+                });
+
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.end(generateScript(dynamicEnvironment));
             });
+        },
+        // очистка outputDir
+        buildStart: () => {
+            rmSync(envOutputDir, { recursive: true, force: true });
+        },
+        // генерация .js для каждого динамического окружения
+        buildEnd: () => {
+            const fullPathToDynamicEnvironments = resolve(process.cwd(), dynamicEnvironmentsDir);
+            readdirSync(fullPathToDynamicEnvironments).forEach((fileName) => {
+                if (!fileName.startsWith('.env.')) {
+                    return;
+                }
 
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            response.end(generateScript(dynamicEnvironment));
-        });
-    },
-    // очистка outputDir
-    buildStart: () => {
-        rmSync(outputDir, { recursive: true, force: true });
-    },
-    // генерация .js для каждого динамического окружения
-    buildEnd: () => {
-        const fullPathToDynamicEnvironments = resolve(process.cwd(), dynamicEnvironmentsDir);
-        readdirSync(fullPathToDynamicEnvironments).forEach((fileName) => {
-            if (!fileName.startsWith('.env.')) {
-                return;
-            }
+                const fullPathToFile = join(fullPathToDynamicEnvironments, fileName);
+                if (statSync(fullPathToFile).isDirectory()) {
+                    return;
+                }
 
-            const fullPathToFile = join(fullPathToDynamicEnvironments, fileName);
-            if (statSync(fullPathToFile).isDirectory()) {
-                return;
-            }
+                const environmentName = getLast(fileName.split('.'));
+                const dynamicEnvironment = getDynamicEnvironment({
+                    ignorePrefixes,
+                    path: fullPathToFile
+                });
 
-            const environmentName = getLast(fileName.split('.'));
-            const dynamicEnvironment = getDynamicEnvironment({
-                ignorePrefixes,
-                path: fullPathToFile
+                if (!existsSync(envOutputDir)) {
+                    mkdirSync(envOutputDir, { recursive: true });
+                }
+
+                const scriptContent = generateScript(dynamicEnvironment);
+
+                writeFileSync(join(envOutputDir, `dynamicEnvironment.${environmentName}.js`), scriptContent);
+                if (FORCE_ENVIRONMENT === environmentName) {
+                    logger.info(
+                        `process.env.FORCE_ENVIRONMENT defined! preparing dynamicEnvironment.js for "${environmentName}"...`
+                    );
+                    forceEnvironmentScriptContent = scriptContent;
+                }
             });
-
-            if (!existsSync(outputDir)) {
-                mkdirSync(outputDir, { recursive: true });
+        },
+        // автоматическое добавление скрипта в html
+        transformIndexHtml: (html) => {
+            if (!html.includes('<head>')) {
+                throw new Error("dynamicEnvironmentsPlugin: '<head>' not found in html");
             }
 
-            writeFileSync(
-                join(outputDir, `dynamicEnvironment.${environmentName}.js`),
-                generateScript(dynamicEnvironment)
-            );
-        });
-    },
-    // автоматическое добавление скрипта в html
-    transformIndexHtml: (html) => {
-        if (!html.includes('<head>')) {
-            throw new Error("dynamicEnvironmentsPlugin: '<head>' not found in html");
+            return html.replace('<head>', `<head><script src='${scriptLink}'></script>`);
+        },
+        writeBundle: () => {
+            if (forceEnvironmentScriptContent) {
+                logger.info(`writing prepared dynamicEnvironment.js in "${outDir}"...`);
+                writeFileSync(join(resolve(outDir, 'dynamicEnvironment.js')), forceEnvironmentScriptContent);
+            }
         }
-
-        return html.replace('<head>', `<head><script src="${scriptLink}"></script>`);
-    }
-});
+    };
+};
 
 export default dynamicEnvironmentsSupport;
